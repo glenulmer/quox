7047b5f337c8d51be1e165ce4c3d5a6de66739f8
glenulmer <glen.ulmer@gmail.com>
2026-05-01 22:23:56 +0200

    not sure again...

--- xcel.0.go
+++ xcel.0.go
@@ -47,9 +47,12 @@ func CreateExcelQuote(qvars QuoteVars_t) (status xlStatus_t) {
 }
 
 func (xl *Excel_t)WriteQuote() (e checkErr_t) {
-	lastBenefitRow, e := xl.WriteBenefitNames(); if e.Err() { return e }
-	e = xl.WriteTipsTitle(lastBenefitRow); if e.Err() { return e }
 	e = xl.WriteClientInfo(); if e.Err() { return e }
+
+	lastBenefitRow, e := xl.WriteBenefitNames(); if e.Err() { return e }
+
+	e = xl.WriteTipsTitle(lastBenefitRow); if e.Err() { return e }
+
 	return checkErr_t{}
 }
 
